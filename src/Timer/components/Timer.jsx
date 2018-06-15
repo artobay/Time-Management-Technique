4c0168f9346e77c592da17baedbacdd5cdb87772
artobay <aliyarfatemi@yahoo.com>
2018-06-14 22:34:57 -0700

    moment is not defined error

--- src/Timer/components/Timer.jsx
+++ src/Timer/components/Timer.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import moment from 'moment';
 import TimerDisplay from './TimerDisplay.jsx';
 import TimerButton from './TimerButton.jsx';
 import TimerConfig from './TimerConfig.jsx';
 
+// we need to the timing so we are going to refactor it to class component
+class Timer extends Component  {
 
-const Timer = () => (
-<div className="card app-content d-block mx-auto" >
-<div className="card-body">
-  <div className="container-fluid">
+      constructor() {
+       super();
+
+        this.state = {
+          currentTime: moment.duration('25', 'minutes'),
+          baseTime: moment.duration('25', 'minutes'),
+        };
+      }
+
+ render()
+   {
+   return  (
+   <div className="card app-content d-block mx-auto" >
+     <div className="card-body">
+       <div className="container-fluid">
       <div className="row">
        <h3 className="d-block mx-auto"> Pomodoro Technique !!! </h3>
-      </div>
-       <TimerDisplay/>
+         </div>
+       <TimerDisplay currentTime={this.state.currentTime}/>
        <TimerButton/>
        <TimerConfig/>
       </div>
   </div>
 </div>
-);
 
+          );
+    }
+ }
 export default Timer;
--- src/Timer/components/TimerDisplay.jsx
+++ src/Timer/components/TimerDisplay.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const TimerDisplay = () => (
+const TimerDisplay = (props) => (
    <div className="row">
-   <h1 className="d-block mx-auto"> 00:00:00 </h1>
+   <h1 className="d-block mx-auto"> { `${props.currentTime.get('hours')}:${props.currentTime.get('minutes')}:${props.currentTime.get('seconds')}` } </h1>
    </div>
 );
 

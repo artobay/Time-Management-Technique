66ced61932720e26c7469bb209a836df5b0d72d7
safari <safari@Qazvin.tto>
2018-07-05 11:21:22 +0430

    add wating method

--- src/Timer/components/Timer.jsx
+++ src/Timer/components/Timer.jsx
@@ -9,7 +9,7 @@ class Timer extends Component {
   constructor() {
     super();
     this.state = { 
-      currentTime: moment().hours(0).minutes(5).seconds(0),
+      currentTime: moment().hours(0).minutes(25).seconds(0),
       baseTime: moment().hours(0).minutes(0).seconds(0),
     };
     // this.setBaseTime = this.setBaseTime.bind(this);
--- src/Timer/components/TimerButton.jsx
+++ src/Timer/components/TimerButton.jsx
@@ -5,34 +5,46 @@ class TimerButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentTime: moment().hours(0).minutes(5).seconds(0),
-      number: 2
+      currentTime: moment().hours(0).minutes(25).seconds(0),
+      countdown: 4,
     };
   }
 
-  handleStart = () => {
-    console.clear();
-    this.state.number = 2;
+  wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+      end = new Date().getTime();
+    }
+  }
+
+  start() {
     this.interval = setInterval(() => {
-      this.setState({ currentTime: this.props.currentTime.subtract(1, "minutes") });
-      if (this.state.currentTime.isBefore(moment({ hour: 0, minute: 0 }))) {
-        this.state.number--;
-        this.setState({ currentTime: this.props.currentTime.add(6, "minutes") });
-        switch (this.state.number) {
-          case 1:
-            this.setState({ currentTime: this.props.currentTime.add(3, "minutes") });
-            break;
-          case 0:
-            clearInterval(this.interval);
-            break;
+      if (!(this.state.currentTime.isBefore(moment({ hour: 0, minute: 1 })))) {
+        this.setState({ currentTime: this.props.currentTime.subtract(1, "minutes") });
+      } else {
+        this.state.countdown--;
+        this.wait(5000);
+        if (this.state.countdown > 1) {
+          this.setState({ currentTime: this.props.currentTime.add(26, "minutes") });
+        } else if (this.state.countdown == 1) {
+          this.setState({ currentTime: this.props.currentTime.add(31, "minutes") });
+        } else if (this.state.countdown <= 0) {
+          clearInterval(this.interval);
+          console.log("Finish");
         }
       }
-    }, 1000);
+    }, 100);
   }
+  handleStart = () => {
+    console.clear();
+    this.start();
+  }
+
   render() {
     return (
       <div>
-        <div>{this.state.number}</div>
+        <div>{this.state.countdown}</div>
         <div className="row">
           <h1 className="d-block mx-auto">
             {this.state.currentTime.format("HH:mm:ss")}  </h1>
